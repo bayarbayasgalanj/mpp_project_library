6fe10a95ffce18db327ff50526400706f3b66d0a
Bayasaa <bayarbayasgalan.j@gmail.com>
2023-03-10 16:07:52 -0600

    missing

--- green_library/src/dataaccess/TestData.java
+++ green_library/src/dataaccess/TestData.java
@@ -120,9 +120,9 @@ public class TestData {
 	@SuppressWarnings("serial")
 	List<User> allUsers = new ArrayList<User>() {
 		{
-			add(new User("101", "xyz", Auth.LIBRARIAN));
-			add(new User("102", "abc", Auth.ADMIN));
-			add(new User("103", "111", Auth.BOTH));
+			add(new User("101", "101", Auth.LIBRARIAN));
+			add(new User("102", "102", Auth.ADMIN));
+			add(new User("103", "103", Auth.BOTH));
 		}
 	};
 }
--- green_library/src/dataaccess/User.java
+++ green_library/src/dataaccess/User.java
@@ -2,14 +2,20 @@ package dataaccess;
 
 import java.io.Serializable;
 
-final public class User implements Serializable {
-	
-	private static final long serialVersionUID = 5147265048973262104L;
+import business.ControllerInterface;
+import business.SystemController;
 
+final public class User implements Serializable {
+	private static final long serialVersionUID = 5147265048973262104L;
+	public static final User INSTANCE = new User();
+	public static boolean isLogin = false;
+	public static Auth loginAuth = null;
+	static ControllerInterface ci = new SystemController();
 	private String id;
 	
 	private String password;
 	private Auth authorization;
+	private User(){}
 	User(String id, String pass, Auth  auth) {
 		this.id = id;
 		this.password = pass;
@@ -29,5 +35,26 @@ final public class User implements Serializable {
 	public String toString() {
 		return "[" + id + ":" + password + ", " + authorization.toString() + "]";
 	}
-	
+	public static User getLoginCheck(String user, String pass){
+		for (User u: ci.allUserObj()){
+			if (u.getId().equals(user) && u.getPassword().equals(pass)){
+				isLogin = true;
+				loginAuth = u.getAuthorization();
+				return u;
+			}
+		}
+		return null;
+	}
+	public static void setNowLogin(boolean log){
+		isLogin = log;
+		if (!isLogin){
+			loginAuth = null;
+		}
+	}
+	public static boolean getNowLogin(){
+		return isLogin;
+	}
+	public Auth getNowAuth(){
+		return loginAuth;
+	}
 }
